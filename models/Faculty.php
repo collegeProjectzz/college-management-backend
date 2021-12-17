e33a92c03ec3fd7a34e4ccdff121ae0ae387cb0a
siddhesh <iamsiddhesh22@gmail.com>
2021-12-18 02:01:10 +0530

    faculty login functionality added

--- models/Faculty.php
+++ models/Faculty.php
@@ -7,6 +7,7 @@ class Faculty
     public $fId;
     public $fName;
     public $fEmail;
+    public $fPassword;
     public $dNo;
 
     public function __construct($db)
@@ -38,5 +39,22 @@ class Faculty
         };
         printf("Error: %s \n", $statement->error);
         return false;
+    }
+
+    public function loginFaculty()
+    {
+        $sql = "SELECT * FROM  Faculty WHERE fId=:fId and fPassword=:fPassword";
+
+        $statement = $this->conn->prepare($sql);
+
+        $this->fId = htmlspecialchars(strip_tags($this->fId));
+        $this->fPassword = htmlspecialchars(strip_tags($this->fPassword));
+
+        $statement->bindParam(':fId', $this->fId);
+        $statement->bindParam(':fPassword', $this->fPassword);
+
+        $statement->execute();
+
+        return $statement;
     }
 }
