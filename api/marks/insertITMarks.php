0a71910cfb05ed38256580507f9779c0ce4ccb49
siddhesh <iamsiddhesh22@gmail.com>
2021-12-18 11:05:40 +0530

    getAllStudentMarks

--- api/marks/insertITMarks.php
+++ api/marks/insertITMarks.php
@@ -1,36 +1,35 @@
 <?php
-
 header('Access-Control-Allow-Origin:*');
 header('Content-Type:application/json');
 header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,application/json,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
 include_once '../../config/DB.php';
-include_once '../../models/Students.php';
+include_once '../../models/Exam.php';
 
 $database = new DB();
 $db = $database->connect();
 
-$post = new Students($db);
+$post = new Exam($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-$post->name = $data->name;
-$post->email = $data->email;
-$post->phone = $data->phone;
-$post->password = $data->password;
-$post->dNo = $data->dNo;
+$post->rollNo = $data->rollNo;
+$post->cId = $data->cId;
+$post->it1 = $data->it1;
+$post->it2 = $data->it2;
 
-if ($post->registerStudent()) {
+
+if ($post->insertITMarks()) {
     echo json_encode(
         array(
-            'message' => 'Student created successfully c'
+            'message' => 'Marks inserted successfully '
         )
     );
 } else {
     echo json_encode(
         array(
-            'message' => 'Error while signing up student'
+            'message' => 'Error while inserting marks'
         )
     );
 }
--- models/Exam.php
+++ models/Exam.php
@@ -16,7 +16,7 @@ class Exam
 
     public function getAllStudentMarks()
     {
-        $sql = "SELECT * FROM Exam";
+        $sql = "SELECT * FROM Exam JOIN Course ON Course.cId=Exam.cId JOIN Students ON Students.rollNo = Exam.rollNo;";
         $statement = $this->conn->prepare($sql);
         $statement->execute();
         return $statement;
@@ -30,5 +30,35 @@ class Exam
         $statement->bindParam(':rollNo', $this->rollNo);
         $statement->execute();
         return $statement;
+    }
+
+    public function insertITMarks()
+    {
+        $sql = "
+        UPDATE 
+            Exam 
+        SET 
+            cId=:cId,
+            it1=:it1,
+            it2=:it2 
+        WHERE rollNo=:rollNo;";
+
+        $statement = $this->conn->prepare($sql);
+
+        $this->rollNo = htmlspecialchars(strip_tags($this->rollNo));
+        $this->cId = htmlspecialchars(strip_tags($this->cId));
+        $this->it1 = htmlspecialchars(strip_tags($this->it1));
+        $this->it2 = htmlspecialchars(strip_tags($this->it2));
+
+        $statement->bindParam(':rollNo', $this->rollNo);
+        $statement->bindParam(':cId', $this->cId);
+        $statement->bindParam(':it1', $this->it1);
+        $statement->bindParam(':it2', $this->it2);
+
+        if ($statement->execute()) {
+            return true;
+        };
+        printf("Error: %s \n", $statement->error);
+        return false;
     }
 }
