d9076704442aa391f8a25ab529bcdd2e768646c7
siddhesh <iamsiddhesh22@gmail.com>
2021-12-20 13:02:32 +0530

    get individual student marks

--- api/exam/getStudentMarks.php
+++ api/exam/getStudentMarks.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 header('Access-Control-Allow-Origin:*');
@@ -6,6 +8,9 @@ header('Content-Type:application/json');
 include_once '../../config/DB.php';
 include_once '../../models/Exam.php';
 
+include_once '../../models/Course.php';
+include_once '../../models/Students.php';
+
 $database = new DB();
 $db = $database->connect();
 
@@ -13,13 +18,35 @@ $post = new Exam($db);
 
 $post->rollNo = isset($_GET['rollNo']) ? $_GET['rollNo'] : die();
 
-$post->getStudentMarks();
+$result = $post->getStudentMarks();
 
-$post_arr = array(
-    'rollNo' => $post->rollNo,
-    'cId' => $post->cId,
-    'it1' => $post->it1,
-    'it2' => $post->it2,
-);
+$course = new Course($db);
+$student = new Students($db);
 
-print_r(json_encode($post_arr));
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $posts_arr = array();
+    $posts_arr['data'] = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $post_item = array(
+            'rollNo' => $rollNo,
+            'cId' => $cId,
+            'it1' => $it1,
+            'it2' => $it2,
+            'cName' => $cName,
+            "credit" => $credit,
+            "name" => $name,
+            "email" => $email,
+            "phone" => $phone,
+            "password" => $password,
+            "dNo" => $dNo,
+        );
+        array_push($posts_arr['data'], $post_item);
+    }
+    echo json_encode($posts_arr);
+} else {
+    echo json_encode(array('message' => "no students found"));
+}
